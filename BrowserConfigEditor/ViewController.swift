7b407cdbe86e7a367beb20aab00f1e7899416197
Gil Burns <gil@gilburns.com>
2025-10-05 07:57:34 -0500

    Initial Commit

---
+++ BrowserConfigEditor/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BrowserConfigEditor
+//
+//  Created by Gil Burns on 10/5/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
