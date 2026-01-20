2c18847a407a58a8ccb6f9863e45ea8f9698daf1
Gil Burns <gil@gilburns.com>
2026-01-20 07:30:03 -0600

    Create Settings.swift

---
+++ BrowserConfigEditor/Views/Settings.swift
@@ -0,0 +1,42 @@
+//
+//  Settings.swift
+//  BrowserConfigEditor
+//
+//  Created by Gil Burns on 1/17/26.
+//
+
+import SwiftUI
+import Sparkle
+
+// This is the view for our updater settings
+// It manages local state for checking for updates and automatically downloading updates
+// Upon user changes to these, the updater's properties are set. These are backed by NSUserDefaults.
+// Note the updater properties should *only* be set when the user changes the state.
+
+struct UpdaterSettingsView: View {
+    private let updater: SPUUpdater
+    
+    @State private var automaticallyChecksForUpdates: Bool
+    @State private var automaticallyDownloadsUpdates: Bool
+    
+    init(updater: SPUUpdater) {
+        self.updater = updater
+        self.automaticallyChecksForUpdates = updater.automaticallyChecksForUpdates
+        self.automaticallyDownloadsUpdates = updater.automaticallyDownloadsUpdates
+    }
+    
+    var body: some View {
+        VStack {
+            Toggle("Automatically check for updates", isOn: $automaticallyChecksForUpdates)
+                .onChange(of: automaticallyChecksForUpdates) {
+                    updater.automaticallyChecksForUpdates = automaticallyChecksForUpdates
+                }
+
+            Toggle("Automatically download updates", isOn: $automaticallyDownloadsUpdates)
+                .disabled(!automaticallyChecksForUpdates)
+                .onChange(of: automaticallyDownloadsUpdates) {
+                    updater.automaticallyDownloadsUpdates = automaticallyDownloadsUpdates
+                }
+        }.padding()
+    }
+}
